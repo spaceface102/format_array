9bd336ee382f667147a9bbd8a3b36ac3571d22af
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-03-25 06:48:51 -0700

    Autocommit at Thu Mar 25 06:48:51 PDT 2021

--- format_array_output.c
+++ format_array_output.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <string.h>
 #include <stdint.h>
 
 void clear_chars(int number);
 void repeat(char c, int number);
-void printarray(void *array, int array_size, int type_size, int max_chars_per_line, char *specifier);
+void printarray(void *array, unsigned long array_size, int type_size, int max_chars_per_line, char *specifier);
 
 int main(void)
 {
-	int array[1000];
+	float array[1000];
 	for(int i = 0; i < 1000; i++)
-		array[i] = i;
-	printarray(array, 1000, sizeof(int), 80, "%d");
+		array[i] = i*1e10;
+	printarray(array, 1000, sizeof(int), 80, "%f");
 }
 
-void printarray(void *array, int array_size, int type_size, int max_chars_per_line, char *specifier)
+void printarray(void *array, unsigned long array_size, int type_size, int max_chars_per_line, char *specifier)
 {
 	/*void pointer for array, to accept any type of array
 	array_size necessary to ONLY access memory related to
@@ -26,7 +25,10 @@ void printarray(void *array, int array_size, int type_size, int max_chars_per_li
 	such as %d, %c, etc.*/
 	
 	//initial check, since code depends heavily on value of specifier
-	if (specifier[0] != '%')
+	//[5] since at most %lld or something to that effect (4 chars + null char)
+	char base_specifier[5], base_spec_len;
+	base_spec_len = specifier_strip(specifier, base_specifier, 5); 
+	if (base_specifier[0] != '%')
 	{
 		printf("Please provide a standard specifier such as %%d, %%f, %%lf, etc!\n");
 		return;
@@ -35,127 +37,124 @@ void printarray(void *array, int array_size, int type_size, int max_chars_per_li
 	//------------------------------------------------------------------------
 	int prev; //num of chars from last print
 	int new_line; //track if new_line
-	int spec_len = strlen(specifier);
-	
+	int (*custom_print)(char *specifier, void *array, unsigned long i);
 	//second specifier check
 	if (spec_len <= 1) //aka < 2
 	{
 		printf("Please make sure to provide more than just %% as specifier!!!\n");
 		return;
 	}
-	//------------------------------------------------------------------------
 
+	//derefrence pointer correctly based on the specifier!
+	//https://www.cplusplus.com/reference/cstdio/printf/ 
+	switch(specifier[1]) //already check specifier[0] && strlen
+	{
+		case 'd': case 'i': 
+			custom_print = print_i;
+			break;
+		case 'u': case 'o': case 'x': case 'X':
+			custom_print = print_ui;
+			break;
+		case 'c': case 's': 
+			/*if 'c', specifier will ensure to print 
+			only one char, and will throw out error if
+			each index of the array is not a character 
+			but character pointer
+			if 's', specifier will ensure string are printed
+			therefore user passed in an array of character 
+			pointers, aka array of strings (think *argv[]) */
+			prev = printf(specifier, ((char *)array)[i]);
+			break;
+		case 'f':
+			prev = printf(specifier, ((float *)array)[i]);
+			break;
+		case 'F': //I will be treating this as double
+			prev = printf(specifier, ((double *)array)[i]);
+			break;
+		case 'e': case 'E'://scientific notation
+			if (type_size == 8) //double is 8 bytes
+				prev = printf(specifier, ((double *)array)[i]);
+			else if (type_size == 4) //float is 4 bytes
+				prev = printf(specifier, ((float *)array)[i]);
+			break;
+		case 'p': //pointer address
+			if (type_size == 8) //64 bit system
+				prev = printf(specifier, ((uint64_t *)array)[i]);
+			else if (type_size == 4) //32 bit system
+				prev = printf(specifier, ((uint32_t *)array)[i]);
+			else if (type_size == 2) //16 bit system
+				prev = printf(specifier, ((uint16_t *)array)[i]);
+			else if (type_size == 1) //8 bit system
+				prev = printf(specifier, ((uint8_t *)array)[i]);
+			break;	
+		case 'h':
+			if(spec_len >= 3) //either 3 or 4 
+			{
+				switch(specifier[2])
+				{
+					case 'd': case 'i':
+						prev = printf(specifier, ((short int *)array)[i]);
+						break;
+					case 'u': case 'o': case 'x': case 'X':
+						prev = printf(specifier, ((unsigned short int *)array)[i]);
+						break; 
+					case 'h':	
+						if(spec_len == 4)
+						{
+							switch(specifier[3])
+							{
+								case 'd': case 'i':
+									prev = printf(specifier, ((signed char *)array)[i]);
+									break;
+								case 'u': case 'o': case 'x': case 'X':
+									prev = printf(specifier, ((unsigned char *)array)[i]);
+									break;
+							}
+						}
+						break;
+				}
+			}
+			break;
+		case 'l':
+			if(spec_len >= 3) //either 3 or 4
+			{
+				switch(specifier[2])
+				{
+					case 'f':
+						prev = printf(specifier, ((double *)array)[i]);
+						break;
+					case 'd': case 'i':
+						prev = printf(specifier, ((long int *)array)[i]);
+						break;
+					case 'u': case 'o': case 'x': case 'X':
+						prev = printf(specifier, ((unsigned long int*)array)[i]);
+						break;
+					case 'l':
+						if (spec_len == 4)
+						{
+							switch(specifier[3])
+							{
+								case 'd': case 'i':
+									prev = printf(specifier, ((long long int *)array)[i]);
+									break;
+								case 'u': case 'o': case 'x': case 'X':
+									prev = printf(specifier, ((unsigned long long int *)array)[i]);
+									break;
+							}
+						}
+						break;
+				}
+			}
+			else //spec_len == 2
+				prev = printf(specifier, ((long int *)array)[i]);
+			break;
+		default:
+			printf("Unknown specifier: %s\n", specifier);
+			return;
+	}
+	//------------------------------------------------------------------------
 	for(int i = 0, j = 0; i < array_size; i++)
 	{
-		//------------------------------------------------------------------------
-		//derefrence pointer correctly based on the specifier!
-		//https://www.cplusplus.com/reference/cstdio/printf/ 
-		switch(specifier[1]) //already check specifier[0] && strlen
-		{
-			case 'd': case 'i': 
-				prev = printf(specifier, ((int *)array)[i]);
-				break;
-			case 'u': case 'o': case 'x': case 'X':
-				prev = printf(specifier, ((unsigned int *)array)[i]);
-				break;
-			case 'c': case 's': 
-				/*if 'c', specifier will ensure to print 
-				only one char, and will throw out error if
-				each index of the array is not a character 
-				but character pointer
-				if 's', specifier will ensure string are printed
-				therefore user passed in an array of character 
-				pointers, aka array of strings (think *argv[]) */
-				prev = printf(specifier, ((char *)array)[i]);
-				break;
-			case 'f':
-				prev = printf(specifier, ((float *)array)[i]);
-				break;
-			case 'F': //I will be treating this as double
-				prev = printf(specifier, ((double *)array)[i]);
-				break;
-			case 'e': case 'E'://scientific notation
-				if (type_size == 8) //double is 8 bytes
-					prev = printf(specifier, ((double *)array)[i]);
-				else if (type_size == 4) //float is 4 bytes
-					prev = printf(specifier, ((float *)array)[i]);
-				break;
-			case 'p': //pointer address
-				if (type_size == 8) //64 bit system
-					prev = printf(specifier, ((uint64_t *)array)[i]);
-				else if (type_size == 4) //32 bit system
-					prev = printf(specifier, ((uint32_t *)array)[i]);
-				else if (type_size == 2) //16 bit system
-					prev = printf(specifier, ((uint16_t *)array)[i]);
-				else if (type_size == 1) //8 bit system
-					prev = printf(specifier, ((uint8_t *)array)[i]);
-				break;	
-			case 'h':
-				if(spec_len >= 3) //either 3 or 4 
-				{
-					switch(specifier[2])
-					{
-						case 'd': case 'i':
-							prev = printf(specifier, ((short int *)array)[i]);
-							break;
-						case 'u': case 'o': case 'x': case 'X':
-							prev = printf(specifier, ((unsigned short int *)array)[i]);
-							break; 
-						case 'h':	
-							if(spec_len == 4)
-							{
-								switch(specifier[3])
-								{
-									case 'd': case 'i':
-										prev = printf(specifier, ((signed char *)array)[i]);
-										break;
-									case 'u': case 'o': case 'x': case 'X':
-										prev = printf(specifier, ((unsigned char *)array)[i]);
-										break;
-								}
-							}
-							break;
-					}
-				}
-				break;
-			case 'l':
-				if(spec_len >= 3) //either 3 or 4
-				{
-					switch(specifier[2])
-					{
-						case 'f':
-							prev = printf(specifier, ((double *)array)[i]);
-							break;
-						case 'd': case 'i':
-							prev = printf(specifier, ((long int *)array)[i]);
-							break;
-						case 'u': case 'o': case 'x': case 'X':
-							prev = printf(specifier, ((unsigned long int*)array)[i]);
-							break;
-						case 'l':
-							if (spec_len == 4)
-							{
-								switch(specifier[3])
-								{
-									case 'd': case 'i':
-										prev = printf(specifier, ((long long int *)array)[i]);
-										break;
-									case 'u': case 'o': case 'x': case 'X':
-										prev = printf(specifier, ((unsigned long long int *)array)[i]);
-										break;
-								}
-							}
-							break;
-					}
-				}
-				else //spec_len == 2
-					prev = printf(specifier, ((long int *)array)[i]);
-				break;
-			default:
-				printf("Unknown specifier: %s\n", specifier);
-				return;
-		}
-		//------------------------------------------------------------------------
 
 		prev += printf(", ");
 		j += prev;
@@ -176,6 +175,28 @@ void printarray(void *array, int array_size, int type_size, int max_chars_per_li
 	}
 }
 
+int specifier_strip(char *specifier, char *base_specifier, int size_base) 
+{
+	/*Ignores flags, width, and .precision specifiers
+	in order to have correct classification in order
+	to 'safely' convert a void pointer and derefrence it*/
+	//size_base == len(base_specifier) which is usually 5
+	int j = 0;
+	for(int i = 0; specifier[i] != '\0' && j < size_base; i++)
+	{
+		switch(specifier[i])
+		{
+			case 'd': case 'i': case 'u': case 'o': case 'x':
+			case 'X': case 'f': case 'f': case 'e': case 'E':
+			case 'c': case 's': case '%': case 'h': case 'l':
+				base_specifier[j++] = specifier[i];
+				break;
+		}
+	}
+	base_specifier[j + 1] = '\0';
+	return j; //return len of base_specifier does the job of strlen
+}
+
 void clear_chars(int number)
 {
 	repeat('\b', number);
@@ -188,3 +209,56 @@ void repeat(char c, int number)
 	for(int i = 0; i < number; i++)
 		printf("%c", c);
 }
+
+
+//----------------LIST OF SLIGHTLY DIFFRENT FUNCS, USED FOR FUNC *-----------------
+int print_int(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((int *)array)[i]);}
+
+int print_uint(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((unsigned int *)array)[i]);}
+
+int print_char(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((char *)array)[i]);}
+
+int print_float(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((float *)array)[i]);}
+
+int print_double(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((double *)array)[i]);}
+
+int print_uint8(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((uint8_t *)array)[i]);}
+
+int print_uint16(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((uint16_t *)array)[i]);}
+
+int print_uint32(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((uint32_t *)array)[i]);}
+
+int print_uint64(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((uint64_t *)array)[i]);}
+
+int print_short_int(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((short int *)array)[i]);}
+
+int print_unsigned_short_int(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((unsigned short int *)array)[i]);}
+
+int print_signed_char(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((signed char *)array)[i]);}
+
+int print_unsigned_char(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((unsigned char *)array)[i]);}
+
+int print_long_int(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((long int *)array)[i]);}
+
+int print_unsigned_long_int(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((unsigned long int *)array)[i]);}
+
+int print_long_long_int(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((long long int *)array)[i]);}
+
+int print_unsigned_long_long_int(char *specifier, void *array, unsigned long i /*index*/)
+	{return printf(specifier, ((unsigned long long int *)array)[i]);}
