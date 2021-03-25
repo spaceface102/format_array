23c582e03c0c9d65793233e935cc97bf3900ebab
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-03-25 01:12:22 -0700

    Autocommit at Thu Mar 25 01:12:22 PDT 2021

--- format_array_output.c
+++ format_array_output.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <ctype.h>
 
+void clear_chars(int number)
+void repeat(char c, int number)
 void format_array_output(void *array, int array_size, int type_size, int max_chars, char *specifier);
 
 int main(void)
@@ -19,4 +21,31 @@ void format_array_output(void *array, int array_size, int type_size, int max_cha
 	directly corresponding to the built in printf specifiers
 	such as %d, %c, etc.*/
 
+	int prev; //num of chars from last print
+	for(int i = 0, j = 0; i < array_size; i++)
+	{
+		prev = printf(specifier, *(array+(i*type_size))) + printf(", ");
+		j += prev;
+		if (j > max_chars)
+		{
+			i--; //need to reprint current index
+			j = 0; //reset char counter for next line
+			clear_chars(prev);
+			printf("\n");
+		}
+	}
+	clear_chars(2); //remove final ", "
+}
+
+void clear_chars(int number)
+{
+	repeat('\b', number);
+	repeat(' ', number);
+	repeat('\b', number);
+}
+
+void repeat(char c, int number)
+{
+	for(int i = 0; i < number; i++)
+		printf("%c", c);
 }
