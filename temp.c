78c635f275d225f6d81f0c41044c5532ac58a613
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-03-25 17:30:21 -0700

    Autocommit at Thu Mar 25 17:30:20 PDT 2021

---
+++ temp.c
@@ -0,0 +1,50 @@
+int void_deref_int(void *array, unsigned long i)
+	{return ((int *)array)[i];}
+
+int void_deref_uint(void *array, unsigned long i)
+	{return ((unsigned int *)array)[i];}
+
+int void_deref_char(void *array, unsigned long i)
+	{return ((char *)array)[i];}
+
+int void_deref_signed_char(void *array, unsigned long i)
+	{return ((signed char *)array)[i];}
+
+int void_deref_unsigned_char(void *array, unsigned long i)
+	{return ((unsigned char *)array)[i];}
+
+int void_deref_float(void *array, unsigned long i)
+	{return ((float *)array)[i];}
+
+int void_deref_double(void *array, unsigned long i)
+	{return ((double *)array)[i];}
+
+int void_deref_uint8(void *array, unsigned long i)
+	{return ((uint8_t *)array)[i];}
+
+int void_deref_uint16(void *array, unsigned long i)
+	{return ((uint16_t *)array)[i];}
+
+int void_deref_uint32(void *array, unsigned long i)
+	{return ((uint32_t *)array)[i];}
+
+int void_deref_uint64(void *array, unsigned long i)
+	{return ((uint64_t *)array)[i];}
+
+int void_deref_short_int(void *array, unsigned long i)
+	{return ((short int *)array)[i];}
+
+int void_deref_unsigned_short_int(void *array, unsigned long i)
+	{return ((unsigned short int *)array)[i];}
+
+int void_deref_long_int(void *array, unsigned long i)
+	{return ((long int *)array)[i];}
+
+int void_deref_unsigned_long_int(void *array, unsigned long i)
+	{return ((unsigned long int *)array)[i];}
+
+int void_deref_long_long_int(void *array, unsigned long i)
+	{return ((long long int *)array)[i];}
+
+int void_deref_unsigned_long_long_int(void *array, unsigned long i)
+	{return ((unsigned long long int *)array)[i];}
