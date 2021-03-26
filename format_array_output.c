6f2aa5725ea7686d25df17f40e88ba7230f22de3
Osbaldo Gonzalez Jr <osbaldogonzalezislc@gmail.com>
2021-03-25 20:20:23 -0700

    Autocommit at Thu Mar 25 20:20:23 PDT 2021

--- format_array_output.c
+++ format_array_output.c
@@ -9,6 +9,7 @@ typedef struct printable_array{
 	unsigned int *shape;
 	unsigned int dimension;
 	char *specifier;
+	char *base_specifier; //no flags, width or .precision info
 } printable_array;
 
 
